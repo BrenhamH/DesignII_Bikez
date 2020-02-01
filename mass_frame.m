ff27d717c64da1787af60aaf7c2422dde51c357c
BrenhamH <48927120+BrenhamH@users.noreply.github.com>
2020-02-01 15:39:22 -0500

    Attributes, modules, and .m
    Initialized attributes, modules, and frame mass .m file

---
+++ mass_frame.m
@@ -0,0 +1,18 @@
+clc
+len_total = [];
+
+len_frame = [];
+
+support_hyp = [];
+
+baxl = [];
+
+back_frame_hyp = [];
+
+backframe_len = [];
+
+frontframe_len = [];
+
+backframe_width = [];
+
+tu_thick = [];
